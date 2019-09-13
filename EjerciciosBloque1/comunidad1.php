7fca03bcf4ef26a55ec67ffc492d90790145b58a
Jonva12 <jonva12@hotmail.com>
2019-09-13 09:57:16 +0200

    bloque basico hasta el 3

---
+++ EjerciciosBloque1/comunidad1.php
@@ -0,0 +1,16 @@
+<html>
+	<head>
+		<title>1. Comunidad</title>
+	</head>
+	<body>
+	<?php
+		$pisos = rand(1,10);
+		$puertas = 4;
+		for($i = 1; $i <= $pisos; $i++){
+			for($j = 1; $j <= $puertas; $j++){
+				echo 'Piso '.$i.' puerta '.$j.'</br>';
+			}
+		}
+	?>
+	</body>
+</html>
