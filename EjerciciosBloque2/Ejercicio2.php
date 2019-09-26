66a84a8ec6895e4fdd7fa91522c43db35f82d184
Jonva12 <jonva12@hotmail.com>
2019-09-26 09:46:39 +0200

    segundo ejercicio bloque2

---
+++ EjerciciosBloque2/Ejercicio2.php
@@ -0,0 +1,25 @@
+<html>
+	<head>
+		<title>2. Tabla</title>
+	</head>
+	<body>
+	<?php
+		$columnas=5;
+		$filas=2;
+
+		function crearTabla($filas, $columnas){
+			echo "<table>";
+			for($i=1;$i<=$filas; $i++){
+				echo "<tr>";
+					for ($j=1; $j <=$columnas ; $j++) { 
+						echo "<td>Columna</td>";
+					}
+				echo "</tr>";
+			}
+			echo "</table>";
+		}
+		crearTabla($filas,$columnas);
+		
+	?>
+	</body>
+</html>
