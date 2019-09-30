572cd9c3fcb27868de9df99eeccd17309450dae9
Jonva12 <jonva12@hotmail.com>
2019-09-30 14:35:52 +0200

    Ejercicio propuesta corregido

--- propuesta.php
+++ propuesta.php
@@ -1,8 +1,9 @@
 <html>
 	<head>
-		<title>7. Ejercicio</title>
+		<title>Calcular Hipotenusa</title>
 	</head>
 	<body>
+        <h1>Calcula la hipotenusa en base a los dos catetos introducidos en los siguientes campos, teniendo en cuenta que es un triangulo rectangulo:</h1>
         <form action="" method="GET">
             Cateto 1:<input type="number" name="Cateto1">
             Cateto 2:<input type="number" name="Cateto2">
@@ -11,7 +12,7 @@
        <?php
             $calcular = calcHipotenusa($_GET['Cateto1'], $_GET['Cateto2']);
             function calcHipotenusa($cateto1, $cateto2){
-                $hipotenusa = (pow($cateto1,2)+pow($cateto2, 2));
+                $hipotenusa = sqrt(pow($cateto1,2)+pow($cateto2, 2));
                 echo("<div>".$hipotenusa."</div");
 
             }
