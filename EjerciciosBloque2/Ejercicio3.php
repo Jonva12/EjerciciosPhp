a1502690a12424b6e30b10e7f1fd665aa2f13c59
Jonva12 <jonva12@hotmail.com>
2019-09-30 12:46:36 +0200

    Ejercicio 3

--- EjerciciosBloque2/Ejercicio3.php
+++ EjerciciosBloque2/Ejercicio3.php
@@ -4,14 +4,21 @@
 	</head>
 	<body>
 	<?php
-		$numeros[];
+		$numeros = [];
+		$suma = 0;
 		for ($i=0; $i < 20; $i++) { 
 			$numeros[$i] = rand(1,20);
+			echo $numeros[$i]."</br>";
+			$suma+=$numeros[$i];
 		}
+		$media = $suma/count($numeros);
 		$min = min($numeros);
 		$max = max($numeros);
-		echo($min."</br>");
-		echo($max."</br>");
+		echo('<div style="color:blue">Minimo '.$min."</div></br>");
+		echo('<div style="color:green">Maximo '.$max."</div></br>");
+		echo("Suma: ".$suma."</br>");
+		echo("Media: ".$media."</br>");
+
 		
 	?>
 	</body>
