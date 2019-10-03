b29bba46d7c0be2db2dd5c0d33e7893eeff5b56b
Jonva12 <jonva12@hotmail.com>
2019-10-03 11:24:29 +0200

    agenda cambios introduccion de datos array

--- agenda.php
+++ agenda.php
@@ -1,33 +1,77 @@
-<?php
-session_start();
-?>
+<!doctype html>
 <html>
 	<head>
-		<title>Agenda PHP</title>
+		<title>
+			DWES02 - Agenda
+		</title>
 	</head>
 	<body>
-		<form action="" method="GET">
-			Nombre:<input type="text" name="nombre">
-			Email:<input type="text" name="email">
-			<input type="submit" name="añadir" value="Añadir">
+		<?php
+			//Funciones extra que he utilizado para gestionar la agenda
+
+			function stringtoarray ($string_agenda,&$array_agenda) {
+				//Convierte la agenda de datos (string) en un array asociativo
+				$a=explode (";",$string_agenda);
+				for($i=0; $i<count($a); $i++) {
+					$array_agenda[$a[$i]]=$a[$i+1];
+					$i++;
+				}
+				return true;
+			}
+
+			function arraytostring ($array_agenda) {
+				//Convierte el array asociativo en una cadena de caracteres cada elemento separado por ;
+				foreach($array_agenda as $key_nombre => $value)
+				{
+				  $string_agenda.=$key_nombre.";".$value.";";
+				}
+				//Quitamos el ultimo ';''
+				$string_agenda=substr($string_agenda, 0, -1);
+				return $string_agenda;
+			}
+
+		?>
+		<?php
+		//Creamos un array para almacenar los datos de la agenda
+		$array_agenda=array();
+
+		if (isset ($_POST['submit'])) {
+			//Rellenamos el array con los datos recibidos en el campo oculto 'agenda' del formulario
+			stringtoarray ($_POST['agenda'],$array_agenda);
+			// REALIZAR COMPROBACIONES NECESARIAS
+			if(!isset($_POST['nombre'])  || $_POST['nombre'] === ""){
+				echo "No has introducido un nombre";
+			}else{
+				if (array_key_exists(strtolower($_POST['nombre']), $array_agenda) && isset($_POST['email'])){
+						unset($array_agenda[strtolower($_POST['nombre'])]);
+						echo "Se ha eliminado el nombre ".$_POST['nombre'];
+				}else{
+					$array_agenda[strtolower($_POST['nombre'])]= $_POST['email'];
+				}
+			}
+		}
+		?>
+
+		<form action="" method="post">
+			<!-- Campo oculto para ir almacenando los elementos de la agenda-->
+			<input name="agenda" type="text" value="<?php echo  arraytostring($array_agenda)?>"/><br>
+			Nombre:<br>
+			<input type="text" name="nombre" value="<?php echo $_POST['nombre']?>"><br><br>
+			Email:<br>
+			<input type="email" name="email" value="<?php echo $_POST['email']?>"><br><br>
+			<button type="submit" name="submit" >Añadir contacto</button>
 		</form>
-		<p id="informacion">Aqui va la info</p>
-	<?php
-		$_SESSION["nombre"] = array();
-		$_SESSION["email"] = array();
 
-		if (array_key_exists(strtolower($_GET['nombre']), $_SESSION["nombre"])) {
-			echo "Nombre ya existente, introduce otro nombre.";
-		}else{
-			array_push($_SESSION["nombre"], strtolower($_GET['nombre']));
-		}
+		<h2>AGENDA</h2>
+		<table>
+		  <tr>
+		    <th>Nombre</th>
+		    <th>Email</th>
+		  </tr>
+		  	<?php
+		  		// recorremos el array y mostramos los contactos
+			?>
+		</table>
 
-		foreach ($_SESSION["nombre"] as $nombre) {
-			echo "<p>".$nombre."</p>";
-		}
-
-
-
-	?>
 	</body>
 </html>
